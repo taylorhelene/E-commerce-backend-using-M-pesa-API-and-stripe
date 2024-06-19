4ca1ef26c1ca34868a09f2b54281e705444dc23b
taylorhelene <taylorhelene09@gmail.com>
2024-06-19 17:19:09 +0300

    mongoose

--- Server.js
+++ Server.js
@@ -136,6 +136,57 @@ app.post('/users', async (req, res) => {
     res.status(400).send({ error: error.message });
   }
 });
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.get('/users/:email', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.email);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.put('/users/:email', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+     return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
   
 
  app.listen(port, async() => {
